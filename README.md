85fd58e56f1c2869775786a0604865ff3e483b49
yanqishi1 <yanqishi1@126.com>
2022-07-05 16:37:11 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# BiliaryAtreais
+
+Biliary Atresia is a server infant disease. The earlier the disease is detected, the better the treatment.
+
+This is a Java Project that help patients to detect Biliary Atreais.
---
+++ src/main/java/com/example/biliaryatresia/BiliaryAtresiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.biliaryatresia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiliaryAtresiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiliaryAtresiaApplication.class, args);
+    }
+
+}
